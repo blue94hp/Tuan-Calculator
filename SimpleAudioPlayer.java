21daaa6efa0aa1eeff699ec8f541b625b928b4be
blue94hp <blue94hp@gmail.com>
2014-12-16 02:18:15 +0700

    Revert "Revert "Calculator""
    This reverts commit e206e96da0a6f9b36d89206b5d0fb8de45407e1f.

---
+++ SimpleAudioPlayer.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Calculator;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.media.*;  
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SimpleAudioPlayer extends JPanel {
+    
+    public SimpleAudioPlayer(URL mediauUrl) {
+        //initComponents();
+        setLayout(new BorderLayout());
+        try {
+            Player mediaPlayer=Manager.createRealizedPlayer(new MediaLocator(mediauUrl));
+            Component video=mediaPlayer.getVisualComponent();
+            Component control=mediaPlayer.getControlPanelComponent();
+            if (video!=null) {
+                add(video, BorderLayout.CENTER);          
+            }
+            add(control, BorderLayout.SOUTH);            
+            //mediaPlayer.start();
+        } catch (Exception ex) {
+        }
+    }
+}
